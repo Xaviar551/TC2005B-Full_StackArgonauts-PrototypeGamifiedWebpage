0a7156b523f8d7f7cacba4f0239ea878040bf18c
Erick Ruiz <EERG05@hotmail.com>
2021-05-24 19:25:55 -0600

    Finally.. all charts works
    Y E S

--- models/Employee.js
+++ models/Employee.js
@@ -1,8 +1,9 @@
-const mysql = require('mysql');
+import mysql from 'mysql'
+
 //Conection with the DB
 var conection = mysql.createConnection({
 	host: 'localhost',
-	database: 'prueba',
+	database: 'cemexevidencia',
 	user: 'root',
 	password: ''
 });
@@ -15,29 +16,22 @@ conection.connect((error) => {
 	}
 });
 
-res = conection.query('SELECT * FROM `empleados_datos`', function (error, res, fileds) {
-	if(error){
-		throw error;
-	}
-	res.forEach(res => {
-		console.log(res)
-	});
-});
 
 export default class EmployeeModel {
   constructor() {
     this.mysqConnection = conection;
   }
-  fetch(params){
+  ID_creator_proyect(params){
     // santize
-    let query = ""
+    /* let query = ""
     for(attr in params){
       query += attr+","
     }
-    query.slice(0, -1)
+    query.slice(0, -1) */
     return new Promise((resolve, reject) =>{
       // add where to query and pass params
-      conection.query('SELECT '+query+' FROM `empleados_datos` ', function (error, res, fileds) {
+      //SELECT edad,SUM(Hijos) as Hijos_total FROM `empleados_datos` GROUP BY edad,Hijos
+      conection.query('SELECT id_creator, count(i.project_key) AS \'Number_projects\' FROM Issue i GROUP BY id_creator', function (error, res, fileds) {
         if(error){
           reject(error);
         }
@@ -45,5 +39,21 @@ export default class EmployeeModel {
         resolve({employees: res})
       });
     });
-  }
+  };
+  Issue_status(){
+    return new Promise((resolve, reject) => {
+      conection.query('SELECT issue_status, COUNT(issue_status) as Total FROM issue GROUP BY issue_status', (error,res,fileds) => {
+        if(error) reject(error);
+        resolve({Estatus_total: res});
+      });
+    });
+  };
+  not_closed(){
+    return new Promise((resolve, reject) => {
+      conection.query('SELECT i.issue_status,COUNT(i.issue_status) as Total FROM Issue i WHERE i.issue_status NOT IN (\'Closed\') GROUP BY issue_status', (error,res,fileds) => {
+        if(error) reject(error);
+        resolve({Not_closed_total: res});
+      });
+    });
+  };
 }
