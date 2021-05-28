925914ae9c75eb4f9a55cd094e73162b543c080c
Erick-Mex <69656531+Erick-Mex@users.noreply.github.com>
2021-05-28 17:54:56 -0600

    Change from require to import

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-const express=require('express');
-const cors=require('cors');
-const cookieParser=require('cookie-parser');
-const mysql = require('mysql');
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import EmployeeController from './controller/Employee.js'
+import EmployeeModel from './models/Employee.js';
+import path from 'path'
 const app = express();
 const router = express.Router();
 const port = 3000;
-const EmployeeController = require('./controller/Employee');
-let EmployeeModel = require('./models/Employee.js');
-
 
 let init_routes = (app, controllerEmployee) => {
   app.use(cookieParser());
@@ -16,16 +15,19 @@ let init_routes = (app, controllerEmployee) => {
   app.use(express.urlencoded({extended: true}));
   
   app.use(express.static('public'));
-  app.use('/css', express.static(__dirname + 'public/css'));
-  app.use('/js', express.static(__dirname + 'public/js'));
-  app.use('/img', express.static(__dirname + 'public/img'));
+  app.use('/css', express.static(path.resolve('./public/css')));
+  app.use('/js', express.static(path.resolve('./public/js')));
+  app.use('/img', express.static(path.resolve('./public/img')));
   
   app.set('views', './views');
   app.set('view engine', 'ejs');
   app.get('/', async (req, res) => { 
-    let params = Object.assing({},req.params, req.body, req.query)
-    let employeeData = await controllerEmployee.get(params)
-    res.render('index', employeeData); 
+    let params = Object.assign({},req.params, req.body, req.query)
+    let ID_creator_proyect = await controllerEmployee.get_ID_creator_proyect(params);
+    let Issue_total = await controllerEmployee.get_Issue_total();
+    let Not_closed = await controllerEmployee.get_not_closed();
+    console.log(Issue_total);
+    res.render('index', {ID_creator_proyect, Issue_total, Not_closed});
   });
   app.get('/About', (req, res) => { res.render('About'); });
   app.get('/Game', (req, res) => {res.render('Game'); });
